cb3c1605947671e7736eb793f04d9dfd7ae4dff9
manjeetsingh47 <7347nmanjeet@gmail.com>
2026-04-10 14:53:59 +0530

    [Manjeet] updated:taskController

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -107,6 +107,23 @@ export const updateTask = async (req, res) => {
 
 
 
+export const getSingleTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.json(task);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
 /*
 DELETE TASK
 */
