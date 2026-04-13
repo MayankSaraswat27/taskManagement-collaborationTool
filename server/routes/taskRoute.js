9bf78df345a4567a4805a2e95433c90542ac45a5
unknown <mayanksaraswat215@gmail.com>
2026-04-13 09:02:51 +0530

    [Mayank] Fixes: taskRoute

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,55 +1,24 @@
 import express from "express";
-
 import {
-    createTask,
-    getTasks,
-    getSingleTask,
-    updateTask,
-    deleteTask
+  createTask,
+  getTasks,
+  getSingleTask,
+  updateTask,
+  deleteTask,
 } from "../controllers/taskController.js";
 
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-
-/*
-CREATE TASK INSIDE LIST
-*/
-
 router.post("/", protect, createTask);
-
-
-/*
-GET TASKS OF A LIST
-Example:
-GET /api/tasks/list/12345
-*/
 
 router.get("/list/:listId", protect, getTasks);
 
-
-/*
-GET SINGLE TASK
-Example:
-GET /api/tasks/12345
-*/
-
 router.get("/:id", protect, getSingleTask);
-
-
-/*
-UPDATE TASK
-*/
 
 router.put("/:id", protect, updateTask);
 
-
-/*
-DELETE TASK
-*/
-
 router.delete("/:id", protect, deleteTask);
-
 
 export default router;
