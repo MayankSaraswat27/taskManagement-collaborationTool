697ae77af13b49c3fee5a3cc50a8a53507d92f22
unknown <mayanksaraswat215@gmail.com>
2026-04-13 09:46:29 +0530

    [Mayank] Fixes: app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,49 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
 import { AppProvider } from "./context/AppContext";
 
-import { Home } from "./pages/Home";
-import { SignUp } from "./pages/SignUp";
-import { Dashboard } from "./pages/Dashboard";
-import { Login } from "./pages/Login"; 
+import "./index.css";
+import "./App.css";
+
+// =======================
+// PROTECTED ROUTE
+// =======================
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+
+  return token ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <AppProvider>
       <BrowserRouter>
         <Routes>
+
+          {/* Public Routes */}
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected Route */}
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+
         </Routes>
       </BrowserRouter>
     </AppProvider>
   );
 }
-  
+
 export default App;
