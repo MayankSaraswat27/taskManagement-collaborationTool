1a2fd1e5c1d05f839e5d64c62fee18ed07519aaf
unknown <mayanksaraswat215@gmail.com>
2026-04-14 12:07:08 +0530

    [Mayank] Fixes : boardRoutes

--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -4,25 +4,22 @@ import {
   createBoard,
   getBoards,
   getSingleBoard,
+  updateBoard,
   addMembers,
   removeMember,
+  deleteBoard,
 } from "../controllers/boardController.js";
 
 import protect from "../middleware/authMiddleware.js";
-import adminOnly from "../middleware/adminMiddleware.js";
-import { deleteBoard } from "../controllers/boardController.js";
 
 const router = express.Router();
 
 router.post("/", protect, createBoard);
-
 router.get("/", protect, getBoards);
-
 router.get("/:id", protect, getSingleBoard);
+router.put("/:id", protect, updateBoard);
 router.delete("/:id", protect, deleteBoard);
-
-router.post("/:id/members", protect, adminOnly, addMembers);
-
-router.delete("/:id/members/:userId", protect, adminOnly, removeMember);
+router.post("/:id/members", protect, addMembers);
+router.delete("/:id/members/:userId", protect, removeMember);
 
 export default router;
