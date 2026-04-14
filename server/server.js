23e5f4d0f379b6a336030860e5017f2d6bcb03cd
unknown <mayanksaraswat215@gmail.com>
2026-04-14 13:28:26 +0530

    Fix CORS for production frontend

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "https://task-management-collaboration-tool-seven.vercel.app/",
     credentials: true,
   })
 );
