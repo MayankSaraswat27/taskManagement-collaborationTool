86b492ac76a6b2e9636fa9514be941a2dbbf9b88
unknown <mayanksaraswat215@gmail.com>
2026-04-14 12:04:29 +0530

    [Mayank] Fixes : userController

---
+++ server/controllers/userController.js
@@ -0,0 +1,36 @@
+import User from "../models/User.js";
+
+export const searchUsers = async (req, res) => {
+  try {
+    const { query } = req.query;
+
+    if (!query) {
+      return res.status(400).json({ message: "Search query is required" });
+    }
+
+    const regex = new RegExp(query, "i");
+
+    const users = await User.find({
+      $or: [{ name: regex }, { email: regex }],
+      _id: { $ne: req.user.id },
+    }).select("name email");
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("name email role");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
